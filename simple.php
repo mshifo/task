3c51fba4000687f6e7158c56b91ac49f65427718
Mahmoud elshafaey <mahmoud@bokdoc.com>
2018-10-30 17:21:51 +0200

    initial commit

---
+++ simple.php
@@ -0,0 +1,29 @@
+<?php
+
+$i = 1;
+while (true) {
+    $page_link = 'https://www.homegate.ch/mieten/immobilien/kanton-zuerich/trefferliste?ep=' . $i;
+    $html = file_get_contents($page_link);
+    if ($html) {
+        $dom = new DOMDocument;
+        @$dom->loadHTML($html);
+        $links = $dom->getElementsByTagName('a');
+        echo $i . ' - <a href="' . $page_link . '">' . $page_link . '</a><br><ol>';
+        foreach ($links as $link) {
+
+            $url = $link->getAttribute('href');
+            if (preg_match("#.*/[0-9]+$#", $url)) {
+                if (filter_var($url, FILTER_VALIDATE_URL)) {
+                    echo '<li><a target="_blank" href="' . $url . '">' . $url . '</a></li>';
+                } else {
+                    echo '<li><a target="_blank" href="https://www.homegate.ch' . $url . '">https://www.homegate.ch' . $url . '</a></li>';
+                }
+            }
+        }
+        echo "</ol>";
+        $i++;
+    } else {
+        echo "pages count = $i";
+        break;
+    }
+}
